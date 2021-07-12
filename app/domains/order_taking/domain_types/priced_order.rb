34741adf3fb505d69f5145a0b1fd270d9eb94760
huyphung1602 <huy.phung.sw@gmail.com>
2021-07-12 23:42:06 +0700

    Add amount to bill method

--- app/domains/order_taking/domain_types/priced_order.rb
+++ app/domains/order_taking/domain_types/priced_order.rb
@@ -2,10 +2,19 @@
 
 module OrderTaking::DomainTypes
   class PricedOrder < T::Struct
+    extend T::Sig
+
     prop :id, Integer
     prop :customer_info, ValidatedCustomerInfo
     prop :shipping_address, String
     prop :billing_address, String
     prop :order_lines, T::Array[OrderLine]
+
+    sig {returns(T.any(Integer, Float))}
+    def amount_to_bill
+      order_lines.reduce(0) do |acc, ol|
+        acc + ol.order_quanity * T.cast(ol.price, Integer)
+      end
+    end
   end
 end
